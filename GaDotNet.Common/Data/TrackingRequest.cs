f16aa7a07aafa9c626646ac6240e7d13f4fc38b0
Jason Spafford <nullprogrammer@gmail.com>
2013-04-12 18:11:52 -0700

    Fixed TrackingRequest doing page view requests
    Page view request were broken because it tried to submit "page" as the
    request type when it did a page view. For some reason, when you submit
    "page" the page view never gets counted. It disapears into the neather.
    However, you don't even need to include it at all because if you don't
    specify a type then the documentation says it will be a page view.
    
    - Removed a ton of duplicated code using three helper functions
    - Fixed TrackingRequest as a page view
    - Changed properties to functions (they should be!)
    - Removed unused code

--- GaDotNet.Common/Data/TrackingRequest.cs
+++ GaDotNet.Common/Data/TrackingRequest.cs
@@ -32,15 +32,10 @@ namespace GaDotNet.Common.Data
 	/// </summary>
 	public class TrackingRequest
 	{
-		public string Referrer;
-		public string ScreenColourDepth;
-		public string AcceptedCharSet;
-		public string AcceptedLanguage;
-
+		public string AnalyticsAccountCode;
 		public string PageTitle;
 		public string PageDomain;
 		public string PageUrl;
-
 		public string ReferralSource = "(direct)";
 		public string Medium = "(none)";
 		public string Campaign = "(direct)";
@@ -48,208 +43,149 @@ namespace GaDotNet.Common.Data
 		public GoogleEvent TrackingEvent;
 		public GoogleTransaction TrackingTransaction;
 
-
-		public string AnalyticsAccountCode;
-
-		//private string rand1 = new Random().Next(10000).ToString();
-		private string timeStampCurrent = GoogleHashHelper.ConvertToUnixTimestamp(DateTime.Now).ToString();
-		private string visitCount = "2";		
+		private readonly string timeStampCurrent = GoogleHashHelper.ConvertToUnixTimestamp (DateTime.Now).ToString ();
+		private const string visitCount = "2";
 
 		protected internal TrackingRequest()
 		{
-			
 		}
 
-		#region Calculated Values
-
-		private int DomainHash
+		public Uri TrackingGifUri
 		{
-			get
-			{
-				if (PageDomain != null)
-				{
-					// converted from the google domain hash code listed here:
-					// http://www.google.com/support/forum/p/Google+Analytics/thread?tid=626b0e277aaedc3c&hl=en
-					int a = 1;
-					int c = 0;
-					int h;
-					char chrCharacter;
-					int intCharacter;
+			get { return new Uri (getTrackingGifURL ()); }
+		}
 
-					a = 0;
-					for (h = PageDomain.Length - 1; h >= 0; h--)
-					{
-						chrCharacter = char.Parse(PageDomain.Substring(h, 1));
-						intCharacter = (int) chrCharacter;
-						a = (a << 6 & 268435455) + intCharacter + (intCharacter << 14);
-						c = a & 266338304;
-						a = c != 0 ? a ^ c >> 21 : a;
-					}
-
-					return a;
-				}
+		private int getDomainHash()
+		{
+			if (PageDomain == null)
 				return 0;
+
+			// converted from the google domain hash code listed here:
+			// http://www.google.com/support/forum/p/Google+Analytics/thread?tid=626b0e277aaedc3c&hl=en
+			int a = 1;
+			int c = 0;
+			int h;
+
+			a = 0;
+			for (h = PageDomain.Length - 1; h >= 0; h--)
+			{
+				int intCharacter = (int)char.Parse (PageDomain.Substring (h, 1));
+				a = (a << 6 & 268435455) + intCharacter + (intCharacter << 14);
+				c = a & 266338304;
+				a = c != 0 ? a ^ c >> 21 : a;
 			}
+			return a;
 		}
-		
-		private string UtmtRequestType
+
+		private string getUtmcCookieString()
 		{
-			get
-			{
-				if (TrackingEvent != null)
-					return "event";
-				if (TrackingTransaction != null)
-					return "transaction";
+			int domainHash = getDomainHash ();
 
-				return "page";
-			}
+			string utma = String.Format ("{0}.{1}.{2}.{3}.{4}.{5}",
+				domainHash,
+				new Random ().Next (1000000000),
+				timeStampCurrent,
+				timeStampCurrent,
+				timeStampCurrent,
+				visitCount);
+
+			//referral informaiton
+			string utmz = String.Format ("{0}.{1}.{2}.{3}.utmcsr={4}|utmccn={5}|utmcmd={6}",
+				domainHash,
+				timeStampCurrent,
+				"1",
+				"1",
+				ReferralSource,
+				Campaign,
+				Medium);
+
+			//return String.Format("__utma%3D{0}.{1}.{2}.{3}.{4}.{5}",)))
+			string utmcc = String.Format ("__utma={0};+__utmz={1};",
+				utma,
+				utmz);
+
+			return (Uri.EscapeDataString (utmcc));
 		}
 
-		//the cookie collection string (we fake it)
-		private string UtmcCookieString  {
-			get
-			{
-
-				Random randomNumber = new Random();
-
-
-
-				string utma = String.Format("{0}.{1}.{2}.{3}.{4}.{5}",
-				                            DomainHash,
-				                            int.Parse(randomNumber.Next(1000000000).ToString()),
-				                            timeStampCurrent,
-				                            timeStampCurrent,
-				                            timeStampCurrent,
-				                            visitCount);
-
-				//referral informaiton
-				string utmz = String.Format("{0}.{1}.{2}.{3}.utmcsr={4}|utmccn={5}|utmcmd={6}",
-				                            DomainHash,
-				                            timeStampCurrent,
-											"1",
-				                            "1",
-											ReferralSource,
-											Campaign,
-											Medium);
-
-				//return String.Format("__utma%3D{0}.{1}.{2}.{3}.{4}.{5}",)))
-				string utmcc = Uri.EscapeDataString(String.Format("__utma={0};+__utmz={1};",
-				                                                  utma,
-				                                                  utmz
-				                                    	));
-
-				return (utmcc);
-			}
-		}
-
-		public Uri TrackingGifUri { get { return new Uri(TrackingGifURL); } }
-		
-		public string TrackingGifURL
+		private string getTrackingGifURL()
 		{
-			get
-			{
-				Random randomNumber = new Random();
+			// REQUEST URL FORMAT:
+			// http://www.google-analytics.com/__utm.gif?utmwv=4.6.5&utmn=488134812&utmhn=facebook.com&utmcs=UTF-8&utmsr=1024x576&utmsc=24-bit&utmul=en-gb&utmje=0&utmfl=10.0%20r42&utmdt=Facebook%20Contact%20Us&utmhid=700048481&utmr=-&utmp=%2Fwebdigi%2Fcontact&utmac=UA-3659733-5&utmcc=__utma%3D155417661.474914265.1263033522.1265456497.1265464692.6%3B%2B__utmz%3D155417661.1263033522.1.1.utmcsr%3D(direct)%7Cutmccn%3D(direct)%7Cutmcmd%3D(none)%3B
 
-				// REQUEST URL FORMAT:
-				// http://www.google-analytics.com/__utm.gif?utmwv=4.6.5&utmn=488134812&utmhn=facebook.com&utmcs=UTF-8&utmsr=1024x576&utmsc=24-bit&utmul=en-gb&utmje=0&utmfl=10.0%20r42&utmdt=Facebook%20Contact%20Us&utmhid=700048481&utmr=-&utmp=%2Fwebdigi%2Fcontact&utmac=UA-3659733-5&utmcc=__utma%3D155417661.474914265.1263033522.1265456497.1265464692.6%3B%2B__utmz%3D155417661.1263033522.1.1.utmcsr%3D(direct)%7Cutmccn%3D(direct)%7Cutmcmd%3D(none)%3B
+			var paramList = new List<KeyValuePair<string, string>>();
 
-				var pageTitleSafe = PageTitle != null
-					? Uri.EscapeDataString (PageTitle)
-					: string.Empty;
+			// Helper functions
+			var addKvp = ((Action<string, string>) ((k, v) =>
+				paramList.Add (new KeyValuePair<string, string> (k, v))));
+			var addEsc = ((Action<string, string>) ((k, v) =>
+				addKvp (k, Uri.EscapeDataString (v))));
+			var addDec = ((Action<string, Decimal>) ((k, v) =>
+				addKvp (k, v.ToString("#.##"))));
 
-				List<KeyValuePair<string, string>> paramList = new List<KeyValuePair<string, string>>
-				{
-					new KeyValuePair<string,string>("utmwv","4.7.2"),										// Analytics version
-					new KeyValuePair<string,string>("utmn",randomNumber.Next(1000000000).ToString()),	// Random request number
-					new KeyValuePair<string,string>("utmhn",PageDomain),								// Your domain name
-					new KeyValuePair<string,string>("utmcs","UTF-8"),									// Document encoding
-					new KeyValuePair<string,string>("utmsr","-"),										// Screen Resolution
-					new KeyValuePair<string,string>("utmsc","-"),										// Screen Resolution
-					new KeyValuePair<string,string>("utmul","-"),										// user language
-					new KeyValuePair<string,string>("utmje","0"),										// java enabled or not
-					new KeyValuePair<string,string>("utmfl","-"),										// user flash version
-					new KeyValuePair<string,string>("utmdt",Uri.EscapeDataString (pageTitleSafe)),				// page title
-					new KeyValuePair<string,string>("utmhid",randomNumber.Next(1000000000).ToString()),										// page title
-					new KeyValuePair<string,string>("utmr","-"),											// referrer URL
-					new KeyValuePair<string,string>("utmp",PageUrl),									// document page URL (relative to root)
-					new KeyValuePair<string,string>("utmac",AnalyticsAccountCode),						// Your GA account code
-					new KeyValuePair<string,string>("utmcc",UtmcCookieString),					// cookie string (encoded)
-					new KeyValuePair<string,string>("utmt",UtmtRequestType)						// type of request (page view or event etc)
-				};
+			var pageTitleSafe = PageTitle != null
+				? Uri.EscapeDataString (PageTitle)
+				: string.Empty;
 
-				//check if our tracking event is null and if not add to the params
-				if (TrackingEvent!=null)
-				{
-					//taken from http://code.google.com/apis/analytics/docs/tracking/gaTrackingTroubleshooting.html
-					string eventString = String.Format("5({0}*{1}*{2})({3}",
-						TrackingEvent.Category,
-						TrackingEvent.Action,
-						TrackingEvent.Label,
-						TrackingEvent.Value
-						);
-					paramList.Add(new KeyValuePair<string, string>("utme", Uri.EscapeDataString(eventString)));
-				}
+			var random = new Random();
+			var utmhidRandom = random.Next (1000000000).ToString();
+			var utmnRandom = random.Next (1000000000).ToString();
 
-				//check if the transaction object is null and if not add the transaction params
-				if (TrackingTransaction!=null)
-				{
-					//taken from http://code.google.com/apis/analytics/docs/tracking/gaTrackingTroubleshooting.html
+			addKvp ("utmwv", "4.7.2");				// Analytics version
+			addKvp ("utmn", utmnRandom);			// Random request number
+			addKvp ("utmhn", PageDomain);           // Your domain name
+			addKvp ("utmcs", "UTF-8");              // Document encoding
+			addKvp ("utmsr", "-");                  // Screen Resolution
+			addKvp ("utmsc", "-");                  // Screen Resolution
+			addKvp ("utmul", "-");                  // user language
+			addKvp ("utmje", "0");                  // java enabled or not
+			addKvp ("utmfl", "-");                  // user flash version
+			addEsc ("utmdt", pageTitleSafe);		// page title
+			addKvp ("utmhid", utmhidRandom);		// page title
+			addKvp ("utmr", "-");					// referrer URL
+			addKvp ("utmp", PageUrl);				// document page URL (relative to root)
+			addKvp ("utmac", AnalyticsAccountCode); // Your GA account code
+			addKvp ("utmcc", getUtmcCookieString());// cookie string (encoded)
 
-					//add product name
-					paramList.Add(new KeyValuePair<string, string>("utmipn", Uri.EscapeDataString(TrackingTransaction.ProductName)));
+			if (TrackingEvent != null) {
+				//taken from http://code.google.com/apis/analytics/docs/tracking/gaTrackingTroubleshooting.html
+				var eventString = String.Format ("5({0}*{1}*{2})({3}",
+					TrackingEvent.Category,
+					TrackingEvent.Action,
+					TrackingEvent.Label,
+					TrackingEvent.Value);
 
-					//add product code/SKU
-					paramList.Add(new KeyValuePair<string, string>("utmipc", Uri.EscapeDataString(TrackingTransaction.ProductSku)));
-
-					//add unit variation (ie Red)
-					paramList.Add(new KeyValuePair<string, string>("utmiva", Uri.EscapeDataString(TrackingTransaction.ProductVariant)));
-
-					//add unit price
-					paramList.Add(new KeyValuePair<string, string>("utmipr", TrackingTransaction.UnitPrice.ToString("#.##")));
-
-					//add quantity
-					paramList.Add(new KeyValuePair<string, string>("utmiqt", TrackingTransaction.Quantity.ToString()));
-
-					//add billing city
-					paramList.Add(new KeyValuePair<string, string>("utmtci", Uri.EscapeDataString(TrackingTransaction.City)));
-					
-					//add billing country
-					paramList.Add(new KeyValuePair<string, string>("utmtco", Uri.EscapeDataString(TrackingTransaction.Country)));
-
-					// add order ID
-					paramList.Add(new KeyValuePair<string, string>("utmtid", TrackingTransaction.OrderID));
-
-					//add shipping cost
-					paramList.Add(new KeyValuePair<string, string>("utmtsp", TrackingTransaction.ShippingCost.ToString("#.##")));
-
-					//add transaction total
-					paramList.Add(new KeyValuePair<string, string>("utmtto", TrackingTransaction.TotalCost.ToString("#.##")));
-
-					//add tax total
-					paramList.Add(new KeyValuePair<string, string>("utmttx", TrackingTransaction.TaxCost.ToString("#.##")));
-				}
-
-
-
-
-
-				//get final param string
-				StringBuilder GaParams = new StringBuilder();
-				foreach (KeyValuePair<string, string> pair in paramList)
-				{
-					GaParams.Append(String.Format("{0}={1}&", pair.Key, pair.Value));
-				}
-				string paramsFinal = GaParams.ToString();
-				paramsFinal = paramsFinal.Substring(0, paramsFinal.Length - 1);
-
-
-				//return the google gif plus all our params
-				return String.Format("http://www.google-analytics.com/__utm.gif?{0}",
-									 paramsFinal);
+				addKvp ("utme", Uri.EscapeDataString (eventString));
+				addKvp ("utmt", "event");
 			}
-		}
-		
 
-		#endregion
+			if (TrackingTransaction != null) {
+				//taken from http://code.google.com/apis/analytics/docs/tracking/gaTrackingTroubleshooting.html
+				addEsc ("utmipn", TrackingTransaction.ProductName);				//add product name
+				addEsc ("utmipc", TrackingTransaction.ProductSku);				//add product code/SKU
+				addEsc ("utmiva", TrackingTransaction.ProductVariant);			//add unit variation (ie Red)
+				addDec ("utmipr", TrackingTransaction.UnitPrice);				//add unit price
+				addKvp ("utmiqt", TrackingTransaction.Quantity.ToString ());	//add quantity
+				addEsc ("utmtci", TrackingTransaction.City);					//add quantity
+				addEsc ("utmtco", TrackingTransaction.Country);					//add billing country
+				addKvp ("utmtid", TrackingTransaction.OrderID);					//add order ID
+				addDec ("utmtsp", TrackingTransaction.ShippingCost);			//add shipping cost
+				addDec ("utmtto", TrackingTransaction.TotalCost);				//add transaction total
+				addDec ("utmttx", TrackingTransaction.TaxCost);					//add tax total
+				addKvp ("utmt", "transaction");
+			}
+
+			//build final param string
+			var GaParams = new StringBuilder();
+			foreach (var pair in paramList) {
+				GaParams.Append (String.Format ("{0}={1}&", pair.Key, pair.Value));
+			}
+
+			//cut off trailing ampersand
+			string paramsFinal = GaParams.ToString ();
+			paramsFinal = paramsFinal.Substring (0, paramsFinal.Length - 1);
+
+			//return the google gif plus all our params
+			return "http://www.google-analytics.com/__utm.gif?" + paramsFinal;
+		}
 	}
 }
